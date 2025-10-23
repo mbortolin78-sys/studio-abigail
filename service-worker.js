17e9f3e55fb749edd7417771bae4dab3359cb96d
mbortolin78-sys <m.bortolin78@gmail.com>
2025-10-23 13:14:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,12 +7,20 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
+// Installazione: salva i file nella cache
 self.addEventListener('install', event => {
+  self.skipWaiting(); // attiva subito
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Attivazione: prende il controllo della pagina
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+// Intercetta richieste e serve dalla cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
