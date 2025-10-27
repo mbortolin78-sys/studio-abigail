c3f11ec5fb3953d036c1f4d04425b7c99d7d3a16
mbortolin78-sys <m.bortolin78@gmail.com>
2025-10-27 18:39:09 +0100

    Update narrativa_server.js

--- server/narrativa_server.js
+++ server/narrativa_server.js
@@ -17,7 +17,7 @@ app.post("/api/comando", async (req, res) => {
   const contenuto = prompt || testo || comando || "Nessun testo ricevuto";
 
   try {
-    const response = await fetch("http://localhost:11434/api/generate", {
+    const response = await fetch("http://127.0.0.1:11434/api/generate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
